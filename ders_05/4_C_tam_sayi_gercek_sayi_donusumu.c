222f86f4d2455e1763c7e85f5dc7e7653488db02
Mesut TOPUZLU <mesuttopuzlu@gmail.com>
2022-01-29 09:51:22 +0300

    Add files via upload

---
+++ ders_05/4_C_tam_sayi_gercek_sayi_donusumu.c
@@ -0,0 +1,12 @@
+/* 
+* C'de tam sayı gerçek sayı dönüşümüne başka bir örnek 
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int sum = 345;
+	int cnt = 17;
+
+	double result = (double)sum / cnt; //operandlardan biri double türüne dönüştürüldüğü için sonuç double olacaktır.
+}
