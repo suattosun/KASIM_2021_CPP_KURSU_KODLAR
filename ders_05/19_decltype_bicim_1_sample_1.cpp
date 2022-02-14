6174c05d108d1fe6a4b8673f1a6eeed8bebf8752
Mesut TOPUZLU <mesuttopuzlu@gmail.com>
2022-02-14 14:05:43 +0300

    Add files via upload

---
+++ ders_05/19_decltype_bicim_1_sample_1.cpp
@@ -0,0 +1,19 @@
+/*
+* decltype ile yapılan tür çıkarımı iki biçimde yapılıyor.
+* Birincisi decltype'ın operandı olan ifade doğrudan isim formunda olacak.
+* Buna ifadenin . ve -> operatörünün operandı olması da dahil.
+* Tür çıkarımında bildirimdeki tür esas alınır.
+*/
+
+int x = 10;
+const int cx = 20;
+int *ptr = &x;
+int &r = x;
+
+int main()
+{
+    decltype(x) a; //a'nın türü int
+    decltype(cx) ca = 10; //ca'nın türü const int (const'luk düşmez). Const olduğundan ilk değer vermek gerekir. 
+    decltype(ptr) p; //p'nin türü int*
+    decltype(r) ref = x; //ref'in türü int&
+}
