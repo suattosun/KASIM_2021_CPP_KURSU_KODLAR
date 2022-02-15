b81a0ff5a114d77e7254180380e2c4f3e54d1da7
AnakinSkywalker77 <97884189+AnakinSkywalker77@users.noreply.github.com>
2022-02-15 11:07:05 +0300

    Delete 5_dynamicStaticPointers.cpp

--- ders_01/5_dynamicStaticPointers.cpp
+++
@@ -1,8 +0,0 @@
-
-int* ptr;	// global hence static ptr is NULL
-
-int main() {
-
-	int* ptr2;			// dynamic pointer ptr2 has indetermined or garbage value
-	static int* ptr3;	// static pointer ptr3 is NULL
-}
